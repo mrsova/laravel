8dad8392bca20b683d352a7a09ccb81ac5d3b0c8
mrsova <serov-pa@yandex.ru>
2017-12-28 10:34:29 +0300

    Шаблонизатор веб пак роуты начало админки

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/admin', 'Admin\DashboardController@index');
+
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
